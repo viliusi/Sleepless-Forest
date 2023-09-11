bc39365aed8886a01a96d6d7d77fd27e7692bbaf
Vilius Ivanovas <ivanovasvilius@gmail.com>
2023-09-11 09:41:08 +0200

    Rebuilding it a bit, have a clear idea of how I wanna go about making it

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set up a path of map parts, make sure they connect well and also make sure you can get which map player is on, and move the camera to the middle of said map
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
