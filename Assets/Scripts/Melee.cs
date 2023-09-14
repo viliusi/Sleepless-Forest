599b3056011ddd45eedc121b762cf74a5514db7e
ruji <rune4057@mail.hansenberg.dk>
2023-09-14 16:00:18 +0200

    Melee hitbox set

---
+++ Assets/Scripts/Melee.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Hitting();
+        }
+    }
+
+    void Hitting()
+    {
+        
+    }
+}
