d311120a476bd68f72f845c9cf0ad1691d8f51a0
ruji <rune4057@mail.hansenberg.dk>
2023-10-24 15:51:29 +0200

    Melee and bullets no longer collide

--- Assets/Scripts/Melee.cs
+++ Assets/Scripts/Melee.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Melee : MonoBehaviour
 {
+    public Transform Player;
+
     void Update()
     {
+        transform.position = Player.position;
+        
         if (Input.GetButtonDown("Fire2"))
         {
             Hitting();
