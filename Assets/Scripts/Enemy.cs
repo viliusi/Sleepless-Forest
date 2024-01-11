4c1ee7a761d7d99a66aff5dfd02d612fdc28b497
Vilius Ivanovas <ivanovasvilius@gmail.com>
2024-01-11 15:38:38 +0100

    Made camera move smoothly and started setting up enemy deactivation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : MonoBehaviour
     public Transform player;
     private Rigidbody2D rb;
     private Vector2 movement;
+    public bool IsActive = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +19,21 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
-
+        if (IsActive)
+        {
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            direction.Normalize();
+            movement = direction;
+        }
     }
     private void FixedUpdate()
     {
-        StartCoroutine(MoveEnemy(movement));
+        if (IsActive)
+        {
+            StartCoroutine(MoveEnemy(movement));
+        }
     }
 
     IEnumerator MoveEnemy(Vector2 direction)
--- Assets/Scripts/Screen.cs
+++ Assets/Scripts/Screen.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class Screen : MonoBehaviour
 {
-
+    public List<GameObject> Enemies = new List<GameObject>();
+    public bool IsUnlocked = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public bool accesible()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        return true;
+        if (collision.gameObject.tag == "EnemyTag")
+        {
+            if (!IsUnlocked)
+            {
+                Enemies.Add(collision.gameObject);
+            }
+        }
+        else if (collision.gameObject.tag == "PlayerTag")
+        {
+            IsUnlocked = true;
+
+            foreach (var enemy in Enemies)
+            {
+                // Find script of enemy and disable being deactivated
+            }
+        }
     }
 }
